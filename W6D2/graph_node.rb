f73c2aeffd8428ed0d7f8471a10413935ec72bab
Jacob Meyer <jacobpmeyer87@gmail.com>
2019-12-24 12:04:01 -0600

    Finish GraphNode homework

--- W6D2/graph_node.rb
+++ W6D2/graph_node.rb
@@ -2,8 +2,44 @@ require "byebug"
 
 class GraphNode
 
+  attr_accessor :neighbors
+  attr_reader :value
+
   def initialize(value)
-    
+    @value = value
+    @neighbors = []
   end
 
+  def inspect
+    "Graph value: #{self.value}"
+  end
+
+end
+
+
+# Examples 
+# a = GraphNode.new('a')
+# b = GraphNode.new('b')
+# c = GraphNode.new('c')
+# d = GraphNode.new('d')
+# e = GraphNode.new('e')
+# f = GraphNode.new('f')
+# a.neighbors = [b, c, e]
+# c.neighbors = [b, d]
+# e.neighbors = [a]
+# f.neighbors = [e]
+
+def bfs(starting_node, target_value)
+  queue = [starting_node]
+  visited = Set.new()
+  until queue.empty?
+    node = queue.shift
+    if node.value == target_value
+      return node.value
+    else
+      queue += node.neighbors unless visited.include?(node)
+      visited.add(node)
+    end
+  end
+  nil
 end
