1d031cd50adc0b8229f5f6c2330f76d3588b0647
Jacob Meyer <jacobpmeyer87@gmail.com>
2020-01-05 21:06:09 -0600

    Finish lrucache homework

--- W7D3/lrucache.rb
+++ W7D3/lrucache.rb
@@ -1,20 +1,55 @@
 class LRUCache
-    def initialize
+    def initialize(size)
+      @max_size = size
+      @cache = []
     end
 
     def count
       # returns number of elements currently in cache
+      cache.length
     end
 
     def add(el)
       # adds element to cache according to LRU principle
+      if max_size == self.count && cache.include?(el)
+        cache.delete(el)
+        cache.push(el)
+      elsif max_size == self.count
+        cache.shift
+        cache.push(el)
+      else
+        cache.push(el)
+      end
     end
 
     def show
       # shows the items in the cache, with the LRU item first
+      p cache
     end
 
     private
     # helper methods go here!
+    attr_reader :max_size
+    attr_accessor :cache
 
   end
+
+  johnny_cache = LRUCache.new(4)
+
+  johnny_cache.add("I walk the line")
+  johnny_cache.add(5)
+
+  johnny_cache.count # => returns 2
+
+  johnny_cache.add([1,2,3])
+  johnny_cache.add(5)
+  johnny_cache.add(-5)
+  johnny_cache.add({a: 1, b: 2, c: 3})
+  johnny_cache.add([1,2,3,4])
+  johnny_cache.add("I walk the line")
+  johnny_cache.add(:ring_of_fire)
+  johnny_cache.add("I walk the line")
+  johnny_cache.add({a: 1, b: 2, c: 3})
+
+
+  johnny_cache.show # => prints [[1, 2, 3, 4], :ring_of_fire, "I walk the line", {:a=>1, :b=>2, :c=>3}]
