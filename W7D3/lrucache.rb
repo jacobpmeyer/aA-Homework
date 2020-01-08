140e9209fa8b580cde4f932edc0d72e6e939f618
Jacob Meyer <jacobpmeyer87@gmail.com>
2020-01-07 18:49:54 -0600

    Implement LRU with time complexity of O(1)

--- W7D3/lrucache.rb
+++ W7D3/lrucache.rb
@@ -1,37 +1,68 @@
+require 'byebug'
+
 class LRUCache
-    def initialize(size)
-      @max_size = size
-      @cache = []
+  attr_reader :max_size
+  attr_accessor :cache
+  def initialize(size)
+    @cache = {}
+    @max_size = size + 2
+    make_head_tail
+  end
+
+  def make_head_tail
+    @cache['head'] = LinkList.new('head', nil, nil)
+    @cache['tail'] = LinkList.new('tail', nil, nil)
+    @cache['head'].prev, @cache['tail'].nex = @cache['tail'], @cache['head']
+  end 
+
+  def count
+    # returns number of elements currently in cache
+    cache.length
+  end
+
+  def add(el)
+    # adds element to cache according to LRU principle
+    self.delete if max_size == self.count - 1
+    head = @cache['head']
+    # debugger
+    if @cache[el] == true
+      @cache[el].nex = head
+      @cache[el].prev = head.prev
+    else
+      @cache[el] = LinkList.new(el, head.prev, head)
+    end
+    head.prev.nex = @cache[el]
+    head.prev = @cache[el]
+  end
+
+  def delete
+    node = @cache['tail'].nex
+    node.prev.nex = node.nex
+    node.nex.prev = node.prev
+    @cache.delete(node.val)
+  end
+
+  def show
+    # shows the items in the cache, with the LRU item first
+    p cache
+  end
+
+  private
+  # helper methods go here!
+  end
+
+
+  class LinkList
+    attr_accessor :val, :prev, :nex
+    def initialize(val, prev, nex)
+      @val = val
+      @prev = prev
+      @nex = nex
     end
 
-    def count
-      # returns number of elements currently in cache
-      cache.length
+    def inspect
+      self.val
     end
-
-    def add(el)
-      # adds element to cache according to LRU principle
-      if max_size == self.count && cache.include?(el)
-        cache.delete(el)
-        cache.push(el)
-      elsif max_size == self.count
-        cache.shift
-        cache.push(el)
-      else
-        cache.push(el)
-      end
-    end
-
-    def show
-      # shows the items in the cache, with the LRU item first
-      p cache
-    end
-
-    private
-    # helper methods go here!
-    attr_reader :max_size
-    attr_accessor :cache
-
   end
 
   johnny_cache = LRUCache.new(4)
@@ -53,3 +84,5 @@ class LRUCache
 
 
   johnny_cache.show # => prints [[1, 2, 3, 4], :ring_of_fire, "I walk the line", {:a=>1, :b=>2, :c=>3}]
+
+  p johnny_cache.cache['tail'].nex
