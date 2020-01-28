31d81a8c1424481db76a6810c0031279397b19d3
Jacob Meyer <jacobpmeyer87@gmail.com>
2020-01-28 09:35:51 -0600

    Finish capybara homework

--- W10D1/music_app/app/controllers/users_controller.rb
+++ W10D1/music_app/app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to user_url(@user.id)
+      redirect_to bands_url
     else
       render :new
     end
