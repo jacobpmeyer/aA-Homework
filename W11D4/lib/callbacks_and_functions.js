2e10848ad237e6c4aa476c684da556c15f9f301c
Jacob Meyer <jacobpmeyer87@gmail.com>
2020-02-07 09:17:43 -0600

    Start canvas intro

---
+++ W11D4/lib/callbacks_and_functions.js
@@ -0,0 +1,16 @@
+// const readline = require('readline');
+
+// const reader = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout
+// });
+
+// window.setTimeout(function() {
+//   alert("HAMMERTIME")
+// }, 5000);
+
+// function hammerTime (time) {
+//   window.setTimeout(function () {
+//     alert(`#{time} is hammertime!`)
+//   }, 2000);
+// }
