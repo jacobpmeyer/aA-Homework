e3e2917a2990d3ccafce02036ca1050840149920
Jacob Meyer <jacobpmeyer87@gmail.com>
2020-02-07 09:41:09 -0600

    Finish canvas intro

--- W11D5/canvas_practice/practice.js
+++ W11D5/canvas_practice/practice.js
@@ -1,3 +1,20 @@
 document.addEventListener("DOMContentLoaded", function(){
+  const canvas = document.getElementById("mycanvas")
 
+  canvas.height = 500;
+  canvas.width = 500;
+
+  const ctx = canvas.getContext("2d");
+
+  ctx.fillStyle = "red";
+  ctx.fillRect(0, 0, 500, 500);
+  ctx.beginPath();
+  ctx.arc(100, 75, 50, 0, 2 * Math.PI);
+  ctx.strokeStyle = "blue";
+  ctx.lineWidth = 20;
+  ctx.stroke();
+  ctx.fillStyle = "green";
+  ctx.fill();
 });
+
+
